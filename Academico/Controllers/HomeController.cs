b44cf8b732a9a1c69e19b0eb9ee24b1536788c3c
MatheusLimaBarbosa <matheus.barbosa.lb@gmail.com>
2026-04-24 11:23:18 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Academico.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Message"] = "Seja bem-vindo às definições de política de privacidade de nossa instituição";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
